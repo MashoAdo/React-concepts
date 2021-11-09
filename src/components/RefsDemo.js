ab8a4f1ad2e881b4dad9622ab0f409dc3a7e996c
Masho Ado <mashoado@gmail.com>
2021-11-09 09:16:50 +0300

    using React.memo to add functionality of pure components to pure components

---
+++ src/components/RefsDemo.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+
+class RefsDemo extends Component {
+    constructor(props) {
+        super(props)
+        this.inputRef = React.createRef()
+    }
+
+    handleClick = () => {
+        alert(this.inputRef.current.value)
+    }
+
+    componentDidMount() {
+        console.log(this.inputRef)
+        // add focus on input element
+        this.inputRef.current.focus()
+    }
+
+
+    render() {
+        return (
+            <div>
+                <input type="text" ref={this.inputRef} placeholder="user name" />
+                <button onClick={this.handleClick} > click </button>
+            </div>
+        )
+    }
+}
+
+export default RefsDemo
